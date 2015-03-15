bcc1b4de4424d92c19e074dd3368c165a6e6d59e
Marc <marc.warrior@gmail.com>
2015-03-14 19:40:20 -0500

    stores pings in lists for each client and gets average pings

--- parsetests.py
+++ parsetests.py
@@ -1,3 +1,4 @@
+import numpy
 import json
 import ast
 from collections import defaultdict
@@ -13,6 +14,7 @@ with open('ipv6_test', 'r') as f:
         data = data.replace('\"+','"')
         data = json.loads(json.loads(ast.literal_eval(data)))
         cdata[data['client']]['ipv6_test'].append(data)
+        test.add(data['client'])
 
 pings = set()
 with open('ipv6_pings', 'r') as f:
@@ -52,21 +54,6 @@ for client in cdata:
         if success:
             enabled.add(client)
 
-olap1 = set()
-for client in test:
-    if client in pings:
-        olap1.add(client)
-
-olap2 = set()
-for client in test:
-    if client in troutes:
-        olap2.add(client)
-
-olap3 = set()
-for client in test:
-    if client in wget:
-        olap3.add(client)
-
 routable = set()
 success = False
 for client in enabled:
@@ -92,12 +79,67 @@ for client in routable:
                 mixed.add(client)
 
 success = False
-for client in hasdns:
+# set of clients who passed ipv4 ping tests
+ping4 = set()
+for client in pings:
     for trial in cdata[client]['ipv6_pings']:
-        print trial['client']
-        print "hello"
-        break
-    break
+        if trial['ipv4_ping']['failed'] == False:
+            ping4.add(client)
+
+# ipv4 ping test speed results
+speed4 = defaultdict(list)
+avg4 = dict()
+avg4list = []
+avg4total = 0
+line = ""
+ind = 0
+for client in ping4:
+    for trial in cdata[client]['ipv6_pings']:
+        try:
+            line = trial['ipv4_ping']['out']
+            while 1:
+                start = line.index('time')+5
+                end = line[start:].index(' ') + start
+                speed4[client].append(float(line[start:end]))
+                line = line[end:]
+
+        except ValueError:
+            pass
+    avg4[client] = numpy.mean(speed4[client])
+    avg4list.append(avg4[client])
+avg4total = numpy.mean(avg4list)
+        
+        
+# set of clients who passed ipv6 ping tests
+ping6 = set()
+for client in pings:
+    for trial in cdata[client]['ipv6_pings']:
+        if trial['ipv6_ping']['failed'] == False:
+            ping6.add(client)
+
+print len(ping6)
+# ipv4 ping test speed results
+speed6 = defaultdict(list)
+avg6 = dict()
+avg6list = []
+avg6total = 0
+line = ""
+ind = 0
+for client in ping6:
+    for trial in cdata[client]['ipv6_pings']:
+        try:
+            line = trial['ipv6_ping']['out']
+            while 1:
+                start = line.index('time')+5
+                end = line[start:].index(' ') + start
+                speed6[client].append(float(line[start:end]))
+                line = line[end:]
+
+        except ValueError:
+            pass
+    avg6[client] = numpy.mean(speed6[client])
+    avg6list.append(avg6[client])
+avg6total = numpy.mean(avg6list)
 
 print "routable: ", len(routable)
 print "have dns: ", len(hasdns)
@@ -105,8 +147,7 @@ print "error set: ", len(errset)
 print "mixed set: ", len(mixed)
 print "test set: ", len(test)
 print "pings set: ", len(pings)
-print "overlap: ",  len(olap1)
 print "troutes set: ", len(troutes)
-print "overlap: ",  len(olap2)
 print "wget set: ", len(wget)
-print "overlap: ",  len(olap3)
+print "ping4: ", len(ping4), ", avg: ", avg4total
+print "ping6: ", len(ping6), ", avg: ", avg6total
