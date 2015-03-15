319dba9938f51d0d58f00fc86699242b20b6f268
Marc <marc.warrior@gmail.com>
2015-03-14 21:26:35 -0500

    parsed wget data

--- parsetests.py
+++ parsetests.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 arr = []
 test = set()
 cdata = defaultdict(lambda:defaultdict(list))
-
+"""
 with open('ipv6_test', 'r') as f:
     for line in f:
         [offset, data] = line.split('\t')
@@ -15,7 +15,7 @@ with open('ipv6_test', 'r') as f:
         data = json.loads(json.loads(ast.literal_eval(data)))
         cdata[data['client']]['ipv6_test'].append(data)
         test.add(data['client'])
-
+"""
 pings = set()
 with open('ipv6_pings', 'r') as f:
     for line in f:
@@ -35,7 +35,6 @@ with open('ipv6_traceroutes', 'r') as f:
         data = json.loads(json.loads(ast.literal_eval(data)))
         cdata[data['client']]['ipv6_traceroutes'].append(data)
         troutes.add(data['client'])
-
 wget = set()
 with open('ipv6_wget', 'r') as f:
     for line in f:
@@ -45,7 +44,7 @@ with open('ipv6_wget', 'r') as f:
         data = json.loads(json.loads(ast.literal_eval(data)))
         cdata[data['client']]['ipv6_wget'].append(data)
         wget.add(data['client'])
-
+"""
 enabled = set()
 success = False
 for client in cdata:
@@ -77,7 +76,7 @@ for client in routable:
                 errset.add(client)
             else:
                 mixed.add(client)
-
+"""
 success = False
 # set of clients who passed ipv4 ping tests
 ping4 = set()
@@ -117,7 +116,6 @@ for client in pings:
         if trial['ipv6_ping']['failed'] == False:
             ping6.add(client)
 
-print len(ping6)
 # ipv4 ping test speed results
 speed6 = defaultdict(list)
 avg6 = dict()
@@ -140,14 +138,58 @@ for client in ping6:
     avg6[client] = numpy.mean(speed6[client])
     avg6list.append(avg6[client])
 avg6total = numpy.mean(avg6list)
+# ipv4 website get results
+ft4 = defaultdict(list) # fetch time list for each client
+ft4avg = dict() # time average for each client
+ft4list = [] # list of averages
+ft4total = 0 # average of averages
+line = ""
+ind = 0
+for client in wget:
+    for trial in cdata[client]['ipv6_wget']:
+        try:
+            line = trial['ipv4_fetch_time']
+            if not line:
+                continue
+            else:
+                ft4[client].append(float(line))
 
+        except KeyError:
+            pass
+    if len(ft4[client]) > 0:
+        ft4avg[client] = numpy.mean(ft4[client])
+        ft4list.append(ft4avg[client])
+ft4total = numpy.mean(ft4list)
+
+# ipv6 website get results
+ft6 = defaultdict(list) # fetch time list for each client
+ft6avg = dict() # time average for each client
+ft6list = [] # list of averages
+ft6total = 0 # average of averages
+line = ""
+ind = 0
+for client in wget:
+    for trial in cdata[client]['ipv6_wget']:
+        try:
+            ft6[client].append(float(trial['ipv6_fetch_time']))
+
+        except KeyError:
+            pass
+    if len(ft6[client]) > 0:
+        ft6avg[client] = numpy.mean(ft6[client])
+        ft6list.append(ft6avg[client])
+ft6total = numpy.mean(ft6list)
+"""
 print "routable: ", len(routable)
 print "have dns: ", len(hasdns)
 print "error set: ", len(errset)
 print "mixed set: ", len(mixed)
-print "test set: ", len(test)
-print "pings set: ", len(pings)
-print "troutes set: ", len(troutes)
-print "wget set: ", len(wget)
-print "ping4: ", len(ping4), ", avg: ", avg4total
-print "ping6: ", len(ping6), ", avg: ", avg6total
+print "test set: ", len(test)"""
+#print "pings set: ", len(pings)
+#print "troutes set: ", len(troutes)
+#print "wget4 set: ", len(ft4)
+#print "wget6 set: ", len(ft6)
+print "ping4: ", len(ping4), ", ping: ", avg4total
+print "ping6: ", len(ping6), ", ping: ", avg6total
+print "wget4: ", len(ft4list), ", fetch time:", ft4total
+print "wget6: ", len(ft6list), ", fetch time:", ft6total
